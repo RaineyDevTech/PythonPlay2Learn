ea2d122ea8329abcaa325b9c02fda18a48bea087
JD <cogscidawg@gmail.com>
2022-09-07 19:24:02 -0400

    timer working, moving to next list, keeps going after timer expires

--- anagram_hunt.py
+++ anagram_hunt.py
@@ -3,8 +3,6 @@ from anagrams import anagrams
 import time
 import datetime
 import random
-from timer import countdown
-
 
 
 lengths = [ 5, 6, 7, 8]
@@ -12,6 +10,9 @@ word_list = []
 word_length = ""
 words_left = ""
 anagram_list = []
+guess_list = []
+first_word = ""
+game_over = False
 
 def start_game():
     pass
@@ -30,8 +31,6 @@ def config_prompt():
     check_length(word_length)
 
     
-
-
 def check_length(wl):
     global word_length
     for length in lengths:
@@ -47,43 +46,83 @@ def check_length(wl):
         config_prompt()
 
 
-
 def get_words(wl):
     global word_list
     word_list = anagrams[int(wl)]
     return word_list
 
 
-def play_game(anlist):
+def play_game(anagram_list):
+    global word_length
+    global words_left
+    global first_word
+    global game_over
 
-    first_word = random.choice(anlist)
-    words_left = len(anlist) - 1
-    print("The word is: " ,first_word)
-    print("There are ", words_left, " unguessed anagrams")
-    print("You have 60 seconds left")
-    guess = input("Make a guess: ")
-    check_guess(guess)
+    words_left = len(anagram_list)
+    
+
+    time_limit = 30
+    start_time = time.time()
+    #print(start_time)
+
+    while not game_over:
+        elapsed_time = time.time() - start_time
+        print(elapsed_time)
+        #print(words_left)
+        
+
+        if words_left == 0:
+            #get_words(word_length)
+            print("this list is solved")
+            get_words(word_length)
+            anagram_list = random.choice(word_list)
+            print(" this list is ", anagram_list)
+            first_word = random.choice(anagram_list)
+            anagram_list.remove(first_word)
+            word_list.remove(anagram_list)
+            play_game(anagram_list)
+
+            
+        print("The word is: " ,first_word)
+        print("There are ", words_left, " unguessed anagrams")
+        print("You have", time_limit - int(elapsed_time), " seconds left")
+        guess = input("Make a guess: ")
+        check_guess(guess)
+
+        if elapsed_time > 30:
+            game_over = True
+            end_game()
+
+        
 
 
 def check_guess(g):
     global anagram_list
+    global words_left
     for word in anagram_list:
 
         if g == word:
             print(g, " is an anagram")
             anagram_list.remove(word)
             print(anagram_list)
-            return anagram_list
+            words_left = len(anagram_list)
+            print(words_left)
+            return
 
     print("wrong you fucking dumbass")
 
 
-
-
-
+def end_game():
+    global game_over
     
+    print("Time is up, Game Over")
+    
+
 def main():
     global anagram_list
+    global first_word
+    global word_list
+    global word_length
 
     config_prompt()
 
@@ -96,15 +135,14 @@ def main():
 
     anagram_list = random.choice(word_list)
     print(" this list is ", anagram_list)
+    word_list.remove(anagram_list)
+    print(word_list)
 
+    first_word = random.choice(anagram_list)
+    anagram_list.remove(first_word)
     play_game(anagram_list)
 
     
-
-
-
-
-
 
 main()
 
